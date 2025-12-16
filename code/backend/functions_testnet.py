c8d2806d7b4dd913d3297dd8f06165d9b9338179
Viktor Vyhnálek <vyhnavik@cvut.cz>
2025-12-16 18:22:54 +0100

    backend: functions_testnet: read testnet args: address to, from

--- code/backend/functions_testnet.py
+++ code/backend/functions_testnet.py
@@ -28,13 +28,10 @@ USDC_ABI = [
 ]
 usdc_contract = w3.eth.contract(address=Web3.to_checksum_address(USDC_ADDRESS), abi=USDC_ABI)
 
-def verify_testnet_transaction(address_from, tx_hash, token, amount):
+def verify_testnet_transaction(address_from, address_to, tx_hash, token, amount):
     # catch errors
-    if not address_from  or not tx_hash or not token or not amount:
+    if not address_from or not address_to or not tx_hash or not token or not amount:
         return {"success": False, "msg": "Missing parameters"}
-
-    if not MY_WALLET:
-        return {"success": False, "msg": "MY_WALLET not configured in .env"}
 
     try:
         receipt = w3.eth.get_transaction_receipt(tx_hash)
@@ -45,7 +42,7 @@ def verify_testnet_transaction(address_from, tx_hash, token, amount):
         tx = w3.eth.get_transaction(tx_hash)
 
         if token == "ETH":
-            if tx['to'].lower() != MY_WALLET.lower():
+            if tx['to'].lower() != address_to.lower():
                 return {"success": False, "msg": f"Wrong recipient wallet {tx['to'].lower()}"}
 
             if tx['from'].lower() != address_from.lower():
@@ -72,7 +69,7 @@ def verify_testnet_transaction(address_from, tx_hash, token, amount):
 
                     found = True
                     break
-x
+
             if not found:
                 return {"success": False, "msg": "Transfer not to your wallet"}
         else:
