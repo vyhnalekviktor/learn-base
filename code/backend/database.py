442f6d7f3f945784af2cc17f1890cf90eb03fb78
Viktor Vyhnálek <vyhnavik@cvut.cz>
2025-12-20 21:35:52 +0100

    backend: add basic database functionality

---
+++ code/backend/database.py
@@ -0,0 +1,113 @@
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_PUBLISHABLE_KEY")
+supabase: Client = create_client(url, key)
+
+# BASIC DB FUNCTIONS
+def init_user_info(wallet: str) :
+    user_info = {
+        "wallet": wallet,
+        "practice_sent": 0,
+        "practice_received": 0,
+        "completed_theory": False,
+        "completed_practice": False,
+        "completed_all": False
+    }
+    response = supabase.table("USER_INFO").insert(user_info).execute()
+    return response.data
+def init_user_progress(wallet: str) :
+    user_progress = {
+        "wallet": wallet,
+        "theory": False,
+        "faucet": False,
+        "sending": False,
+        "receiving": False,
+        "mint": False,
+        "launch": False
+    }
+    response = supabase.table("USER_PROGRESS").insert(user_progress).execute()
+    return response.data
+
+def get_user_info(wallet: str):
+    response = (
+        supabase.table("USER_INFO").select("*").eq("wallet", wallet)
+        .maybe_single()  # returns Dict / None
+        .execute()
+    )
+    return response.data
+
+def get_user_progress(wallet: str):
+    response = (
+        supabase.table("USER_PROGRESS").select("*").eq("wallet", wallet).maybe_single().execute()
+    )
+    return response.data
+
+def get_field(table_name: str, field_name: str, wallet: str):
+    response = (
+        supabase.table(table_name)
+        .select(field_name).eq("wallet", wallet).maybe_single() .execute()
+    )
+    row = response.data
+    if row is None:
+        return None
+    return row[field_name]
+
+def update_field(table_name: str, field_name: str, wallet: str, value):
+    try:
+        response = (
+            supabase.table(table_name).update({field_name: value}).eq("wallet", wallet).execute()
+        )
+        return response.data
+    except Exception as e :
+        return None
+# SOPHISTICATED DB FUNCTIONS
+#returns: ([{'wallet': 'test_user', 'id': 18, 'created_at': '2025-12-20T20:16:41.898289+00:00', 'practice_sent': 0, 'practice_received': 0, 'completed_all': False, 'completed_theory': False, 'completed_practice': False}], [{'id': 11, 'wallet': 'test_user', 'created_at': '2025-12-20T20:16:41.997225+00:00', 'theory': False, 'faucet': False, 'sending': False, 'receiving': False, 'mint': False, 'launch': False}])
+def add_user(wallet: str) :
+    try:
+        row_info = init_user_info(wallet)
+        row_progress = init_user_progress(wallet)
+        return row_info, row_progress
+
+    except Exception as e :
+        return None
+
+#returns: ({'wallet': 'test_user', 'id': 18, 'created_at': '2025-12-20T20:16:41.898289+00:00', 'practice_sent': 0, 'practice_received': 0, 'completed_all': False, 'completed_theory': False, 'completed_practice': False}, {'id': 11, 'wallet': 'test_user', 'created_at': '2025-12-20T20:16:41.997225+00:00', 'theory': False, 'faucet': False, 'sending': False, 'receiving': False, 'mint': False, 'launch': False})
+def get_user(wallet: str):
+    try:
+        info = get_user_info(wallet)
+        progress = get_user_progress(wallet)
+        return info, progress
+    except Exception as e :
+        return None
+
+#returns: {'progress_deleted': [{'id': 11, 'wallet': 'test_user', 'created_at': '2025-12-20T20:16:41.997225+00:00', 'theory': False, 'faucet': False, 'sending': False, 'receiving': False, 'mint': False, 'launch': False}], 'info_deleted': [{'wallet': 'test_user', 'id': 18, 'created_at': '2025-12-20T20:16:41.898289+00:00', 'practice_sent': 0, 'practice_received': 0, 'completed_all': False, 'completed_theory': False, 'completed_practice': False}]}
+def delete_user(wallet: str) :
+    try:
+        prog_resp = (
+            supabase.table("USER_PROGRESS")
+            .delete().eq("wallet", wallet).execute()
+        )
+        info_resp = (
+            supabase.table("USER_INFO")
+            .delete().eq("wallet", wallet).execute()
+        )
+
+        return {
+            "progress_deleted": prog_resp.data,
+            "info_deleted": info_resp.data,
+        }
+    except Exception as e :
+        return None
+
+print(add_user("test_user"))
+print(get_field("USER_INFO", "practice_sent", "test_user"))
+print(get_user("sdfasfafar"))
+print(get_user("test_user"))
+print(add_user("test_user"))
+print(delete_user("test_user"))
+print(update_field("test_user", "wallet", "test_user", "xxx"))
