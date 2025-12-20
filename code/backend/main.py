517c130c6bee9e8232e0b58225a8a147a2b004d1
Viktor Vyhnálek <vyhnavik@cvut.cz>
2025-12-20 21:36:14 +0100

    backend: add basic database endpoints

--- code/backend/main.py
+++ code/backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
 from dotenv import load_dotenv
-import functions_testnet, functions_mainnet
+import functions_testnet, functions_mainnet, database
 
 load_dotenv()
 app = FastAPI()
@@ -83,3 +83,94 @@ async def testnet_send(request: Request):
 
     #after getting wallet
     #return send
+
+@app.post("/api/database/init-user")
+async def api_init_user(request: Request):
+    data = await request.json()
+    wallet = data.get("wallet")
+    if not wallet:
+        raise HTTPException(status_code=400, detail="No wallet!")
+
+    exists = database.get_user(wallet)[0].get("wallet")
+    if exists:
+        return {"success": True}
+
+    response = database.add_user(wallet)
+    if response.get("message"):
+        raise HTTPException(status_code=400, detail="Error adding user to DB.")
+
+    return {"success": True}
+
+@app.post("/api/database/delete-user")
+async def api_del_user(request: Request):
+    data = await request.json()
+    wallet = data.get("wallet")
+    if not wallet:
+        raise HTTPException(status_code=400, detail="No wallet!")
+    response = database.remove_user(wallet)
+    if not response:
+        raise HTTPException(status_code=400, detail="Error deleting user from DB.")
+    return {"success": True}
+
+#returns: ({'wallet': 'test_user', 'id': 18, 'created_at': '2025-12-20T20:16:41.898289+00:00', 'practice_sent': 0, 'practice_received': 0, 'completed_all': False, 'completed_theory': False, 'completed_practice': False}, {'id': 11, 'wallet': 'test_user', 'created_at': '2025-12-20T20:16:41.997225+00:00', 'theory': False, 'faucet': False, 'sending': False, 'receiving': False, 'mint': False, 'launch': False})
+@app.post("/api/database/get-user")
+async def get_user(request: Request):
+    data = await request.json()
+    wallet = data.get("wallet")
+    if not wallet:
+        raise HTTPException(status_code=400, detail="No wallet!")
+
+    info, progress = database.get_user(wallet)
+    if not info or not progress:
+        raise HTTPException(status_code=400, detail="Error getting user from DB.")
+    return {"success": True, "info": info, "progress": progress}
+
+@app.post("/api/database/update_field")
+async def update_field(request: Request):
+    data = await request.json()
+    wallet = data.get("wallet")
+    table_name = data.get("table_name")
+    field_name = data.get("field_name")
+    value = data.get("value")
+
+    if not wallet or not table_name or not field_name or not value:
+        raise HTTPException(status_code=400, detail="Invalid parameters!")
+
+    response = database.update_field(table_name, field_name, wallet, value)
+    if not response:
+        raise HTTPException(status_code=400, detail="Error updating field in DB.")
+    return {"success": True}
+
+@app.post("/api/database/get-field")
+async def get_field(request: Request):
+    data = await request.json()
+    wallet = data.get("wallet")
+    table_name = data.get("table_name")
+    field_name = data.get("field_name")
+    if not wallet or not table_name or not field_name:
+        raise HTTPException(status_code=400, detail="Invalid parameters!")
+
+    response = database.get_field(table_name, field_name, wallet)
+    if not response:
+        raise HTTPException(status_code=400, detail="Error getting field from DB.")
+    return {"success": True, "value": response}
+
+
+#is user eligible for receiving USDC on testnet? did he pay more than withdraw?
+@app.post("/api/database/eligible-rec")
+async def eligible_rec(request: Request):
+    data = await request.json()
+    wallet = data.get("wallet")
+    if not wallet:
+        raise HTTPException(status_code=400, detail="No wallet!")
+
+    sent = database.get_field("USER_INFO", "practice_sent", "wallet")
+    received = database.get_field("USER_INFO", "practice_received", "wallet")
+
+    if not sent or not received:
+        raise HTTPException(status_code=400, detail="Error getting user data from DB.")
+
+    if received >= sent:
+       return {"success":True, "eligible": False}
+    return {"success":True, "eligible":True}
+
